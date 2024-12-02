947526a611803646aeb45e12c1da260263fc93e9
Gustav Toft <gtoft98@gmail.com>
2024-12-02 18:30:23 +0100

    Updated model evaluation.

--- datasets.py
+++ datasets.py
@@ -42,35 +42,31 @@ class FrameImageDataset(torch.utils.data.Dataset):
 
 
 class FrameVideoDataset(torch.utils.data.Dataset):
-    def __init__(self, 
-    root_dir = 'ufc10', 
-    split = 'train', 
-    transform = None,
-    stack_frames = True
-):
-
-        self.video_paths = sorted(glob(f'{root_dir}/videos/{split}/*/*.avi'))
-        self.df = pd.read_csv(f'{root_dir}/metadata/{split}.csv')
+    def __init__(self, root_dir='ufc10', split='train', transform=None, stack_frames=True):
+        self.root_dir = root_dir
+        self.video_paths = sorted(glob(os.path.join(root_dir, 'frames', split, '*', '*')))  # Path to frame folders
+        self.df = pd.read_csv(os.path.join(root_dir, 'metadata', f'{split}.csv'))
         self.split = split
         self.transform = transform
         self.stack_frames = stack_frames
-        
         self.n_sampled_frames = 10
 
     def __len__(self):
         return len(self.video_paths)
-    
+
     def _get_meta(self, attr, value):
-        return self.df.loc[self.df[attr] == value]
+        matches = self.df[self.df[attr] == value]
+        if len(matches) == 0:
+            raise ValueError(f"No metadata found for {attr}={value}")
+        return matches.iloc[0]['label']  # Return just the label value directly
 
     def __getitem__(self, idx):
-        video_path = self.video_paths[idx]
-        video_name = video_path.split('/')[-1].split('.avi')[0]
-        video_meta = self._get_meta('video_name', video_name)
-        label = video_meta['label'].item()
+        frame_dir = self.video_paths[idx]
+        video_name = os.path.basename(frame_dir)  # Get the video name from the frame directory
+        label = self._get_meta('video_name', video_name)
 
-        video_frames_dir = self.video_paths[idx].split('.avi')[0].replace('videos', 'frames')
-        video_frames = self.load_frames(video_frames_dir)
+        # Load frames directly from the frame directory
+        video_frames = self.load_frames(frame_dir)
 
         if self.transform:
             frames = [self.transform(frame) for frame in video_frames]
@@ -80,7 +76,6 @@ class FrameVideoDataset(torch.utils.data.Dataset):
         if self.stack_frames:
             frames = torch.stack(frames).permute(1, 0, 2, 3)
 
-
         return frames, label
     
     def load_frames(self, frames_dir):
@@ -89,9 +84,7 @@ class FrameVideoDataset(torch.utils.data.Dataset):
             frame_file = os.path.join(frames_dir, f"frame_{i}.jpg")
             frame = Image.open(frame_file).convert("RGB")
             frames.append(frame)
-
         return frames
-
 
 if __name__ == '__main__':
     from torch.utils.data import DataLoader
@@ -120,4 +113,5 @@ if __name__ == '__main__':
 
     for video_frames, labels in framevideostack_loader:
         print(video_frames.shape, labels.shape) # [batch, channels, number of frames, height, width]
+            
             
